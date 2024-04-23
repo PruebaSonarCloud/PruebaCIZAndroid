10108082e0efd691c0c032744e8cf600214dec09
Zeydi <167125203+Zeydi@users.noreply.github.com>
2024-04-23 12:01:36 -0600

    [ADD] Tests

---
+++ app/src/main/java/com/example/pruebacizandroid/ExampleViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.pruebacizandroid
+
+class ExampleViewModel {
+    fun getString(str: String) : String {
+        return str
+    }
+}
--- app/src/test/java/com/example/pruebacizandroid/ExampleUnitTest.kt
+++ app/src/test/java/com/example/pruebacizandroid/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.example.pruebacizandroid
 
+import kotlinx.coroutines.Dispatchers
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,16 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private lateinit var exampleViewModel: ExampleViewModel
+
+    @Before
+    fun onBefore(){
+        exampleViewModel = ExampleViewModel()
+    }
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testStringFun() {
+        val str = exampleViewModel.getString("Zeydi")
+        assertEquals("El valor del no coincide", str, "Zeydi") //Si se desea imprimir un mensaje
     }
 }
